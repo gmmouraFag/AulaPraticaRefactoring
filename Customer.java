c082e16248eea6d4f698fd852dc1547376ca6c67
gmmouraFag <gmmoura@mminha.fag.edu.br>
2024-11-28 21:10:06 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -26,34 +26,42 @@ public class Customer {
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
         }
 
-        // add footer lines
         result += "Amount owed is " + getTotalCharge() + "\n";
         result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+        return result;
+    }
 
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getMovie().getTitle() + ": " + each.getCharge() + "<BR>\n";
+        }
+
+        result += "<P>You owe <EM>" + getTotalCharge() + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + getTotalFrequentRenterPoints() + "</EM> frequent renter points<P>";
         return result;
     }
 
     private double getTotalCharge() {
         double result = 0;
         Enumeration rentals = _rentals.elements();
-
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             result += each.getCharge();
         }
-
         return result;
     }
 
     private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration rentals = _rentals.elements();
-
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             result += each.getFrequentRenterPoints();
         }
-
         return result;
     }
 }
